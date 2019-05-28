4e4468710851bc046070e0a50b2d5d1804397543
kkkjerry <jaeyeol2931@gmail.com>
2019-05-29 02:22:15 +0900

    Update

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # MRI_translation
+
+- Interpolation MRI T1 Image
+- Figure of Flow
+
+<img src="./figure/Net.png" width=50% height=50%>
+
+
+## Network Architecture
+- Backbone : VGG16, Resnet, Xepction, MobileNet
+
+## Training Method
+- Contrast Loss : Mean Square Error
+- Context Loss  : Mutual Information (Not yet...) 
+
+## To do list
+- [x] Reading Data
+- [x] Build Network Architecture
+- [ ] Define Loss Function
+- [ ] Making Generator 
+- [ ] Training Netrowk
+
--- utils.py
+++ utils.py
@@ -43,3 +43,43 @@ def load_nii_multi(PATH):
     t2_img = load_nii(t2_PATH)
     
     return t1_img, dante_img, t2_img
+
+
+from keras.applications import VGG16, ResNet50, Xception, MobileNet
+from keras.applications import DenseNet121, NASNetMobile, InceptionResNetV2, InceptionV3
+def load_base_model(backbone = 'vgg'):
+    '''
+    Loading backbone network
+    ====== Input ======
+    backbone : Network name of backbone 
+        ['vgg', 'resnet', 'xception', 'mobile', 'IRv2', 'Iv3', 'dense', 'nas']
+    weights : Path of Pretrained weights 
+    include_top : whether to include the classifier(the 3 fully-connected layers)
+    
+    ====== Output ======
+    base_model : Keras Model instance
+    '''
+    model_dict = {'vgg':'VGG16',
+                 'resnet':'ResNet50',
+                 'xception':'Xception',
+                 'mobile':'MobileNet',
+                 'IRv2':'InceptionResNetV2',
+                 'Iv3':'InceptionV3',
+                 'dense':'DenseNet121',
+                 'nas':'NASNetMobile'}
+    
+    command = "base_model = %s(weights=None, include_top=False)"%model_dict[backbone]
+    print("Loading %s model"%model_dict[backbone])
+    exec(command, globals())
+    non_train_params = [layer.shape.num_elements() for layer in base_model.non_trainable_weights]
+    non_train_params = sum(non_train_params)
+    print("Total Parameter of Model : ", format(base_model.count_params(), ','))
+    print("Trainable Parameter of Model : ", format(base_model.count_params()-non_train_params, ','))
+    print("Non-Trainable Parameter of Model : ", format(non_train_params, ','))
+    return base_model
+
+from keras import backend as K
+
+def layer_mean(x):
+    x = K.mean(x, axis=-1, keepdims=True)
+    return x
